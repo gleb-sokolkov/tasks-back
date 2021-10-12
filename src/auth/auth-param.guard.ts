98b62f6798ed3819a245b44c94087a98ae6faa1e
gleb-sokolkov <zmihpog@gmail.com>
2021-10-13 00:13:11 +0600

    added authparamguard

---
+++ src/auth/auth-param.guard.ts
@@ -0,0 +1,27 @@
+import { JwtService } from '@nestjs/jwt';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthParamGuard implements CanActivate {
+  constructor(private jwtService: JwtService) {}
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const req = context.switchToHttp().getRequest();
+
+    if (!req.user) {
+      throw new UnauthorizedException({
+        message: 'Некорректный токен',
+      });
+    }
+
+    return req.user.id === parseInt(req.params.user_id);
+  }
+}
